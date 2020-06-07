b23483c07f4da05aac25c92037d59275f4594456
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-07 04:02:55 -0400

    update; add headroom for future navbar

--- dev-website/src/app/app.component.ts
+++ dev-website/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import Headroom from "headroom.js";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dev-website';
+export class AppComponent implements OnInit {
+  title = 'Exordium Development';
+
+  ngOnInit (): void {
+    /*
+    const header = document.querySelector("header");
+    const headroom = new Headroom(header);
+    headroom.init();
+    */
+  }
 }
