d7656bdf8e5474a5432f6458026a7f9fc384cddd
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-07 04:01:54 -0400

    update; hashtag strat and components

--- dev-website/src/app/app.module.ts
+++ dev-website/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy  } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
