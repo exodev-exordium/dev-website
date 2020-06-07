a5e039417f3f6174ac4d16f058ff1383bf10de96
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-07 04:02:39 -0400

    update; add routing for welcome page

--- dev-website/src/app/app-routing.module.ts
+++ dev-website/src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'welcome',
+    pathMatch: 'full',
+    data: {
+      title: 'Exordium Development'
+    }
+  },
+  {
+    path: 'welcome',
+    component: WelcomeComponent,
+    data: {
+      title: 'Welcome'
+    }
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
